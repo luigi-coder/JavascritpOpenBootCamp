659c27527c387e57725a06a3626fb937dbac9848
luigi-coder <luigicoder28@gmail.com>
2022-10-19 13:08:18 -0300

    Tarea 02

---
+++ Tarea02/main.js
@@ -0,0 +1,10 @@
+const name = "Luis";
+const age = 30;
+const developer = true;
+const birth = new Date(1992, 1, 25);
+const favoriteBook = {
+    title: "HHhH",
+    author: "Laurent Binet",
+    date: new Date(2013, 08, 06),
+    url: "https://www.planetadelibros.com/autor/laurent-binet/000026236"
+}
