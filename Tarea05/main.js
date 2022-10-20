f685304738b885030f049c94fc220776a43b389c
luigi-coder <luigicoder28@gmail.com>
2022-10-20 09:36:47 -0300

    Tarea 05 Numbers

---
+++ Tarea05/main.js
@@ -0,0 +1,25 @@
+// Una variable que contenga tu altura en centímetros (entero)
+let altura_cm = 168;
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+let altura_m = altura_cm/100;
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = new Number(80);
+let peso_float = peso.toFixed(2);
+
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+let alt_redon_arriba = Math.ceil(altura_m);
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let alt_redon_abajo = Math.floor(altura_m);
+
+// variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let max_js = Number.MAX_VALUE;
+let max_js_mas_1 = max_js + 1;
+let max_js_igual = max_js == max_js_mas_1;
+
+
+
+
